43b74f1403a0de410c37683807b0eb98e2f6e2f4
brainsoap <vjaszai@brainsum.com>
2018-04-09 22:06:25 +0200

    Menu items have active and visited state now

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -6,19 +6,35 @@ functions.ready(() => {
   const lang = window.GDPR.lang.toUpperCase();
   const baseURL = window.GDPR.baseURL;
 
-  let content = document.querySelector('#gdpr-toc .content');
   window.frames['eurlex'].location = 'https://eur-lex.europa.eu/legal-content/' + lang + '/TXT/?uri=celex:32016R0679';
 
   // TOC link clicks
-  content.addEventListener('click', (event) => {
-    if(event.target.tagName === 'A') {
-      let id = event.target.href.split('#')[1];
-      window.frames['eurlex'].location = event.target.href;
+  (function () {
+    let content = document.querySelector('#gdpr-toc .content');
+
+    content.addEventListener('click', (event) => {
+      if(event.target.tagName === 'A') {
+        let id = event.target.href.split('#')[1];
+        window.frames['eurlex'].location = event.target.href;
+  
+        // Adding classes to links
+        removeClassFromAll('active');
+        event.target.classList.add('active');
+      }
+
+      event.preventDefault(); 
+      return false;
+    });
+
+    function removeClassFromAll(className) {
+      let links = content.querySelectorAll('a').forEach((link) => {
+        if(link.classList.contains('active')) {
+          link.classList.add('visited');
+        }
+        link.classList.remove(className);
+      });
     }
-    
-    event.preventDefault(); 
-    return false;
-  });
+  })();
 
   // Language selector
   let languageSelect = document.getElementById('languages');
