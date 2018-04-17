864e53d4c63935221c24ff7faeb1194e427c7949
brainsoap <vjaszai@brainsum.com>
2018-04-17 16:16:18 +0200

    Major refactor. Links have now english title in data attribute.

--- toc-scraper/index.js
+++ toc-scraper/index.js
@@ -3,20 +3,24 @@ const path = require('path');
 const GDPR = require('./modules/gdpr');
 
 const languages = ['EN','HU','DE'];
-const eusite = 'http://eur-lex.europa.eu/legal-content/{{lang}}/TXT/HTML/?uri=CELEX:32016R0679';
+const url = 'eur-lex.europa.eu/legal-content/{{lang}}/TXT/HTML/?uri=CELEX:32016R0679';
 global.__basedir = __dirname;
 
-languages.forEach((language) => {
-  let gdpr = new GDPR(language);
-    gdpr.scrapeURL(eusite.replace('{{lang}}', language)).then((scrape) => {
-      let toc = gdpr.formatContent(gdpr.structureContent(scrape));
-      let tocFile = path.join(__dirname, '..', 'web', 'dist', 'inc', 'parts', `toc_${language.toLowerCase()}.html`);
-      fs.writeFile(tocFile, toc, (err) => {
-        if(err) throw err;
-        console.log(`TOC (${language}) file has been written to "${tocFile}".`);
-      });
-    }).catch((error) => {
-    console.log(error);
-  });
-});
 
+let gdpr = new GDPR(url, languages);
+
+gdpr.scrapeURLS().then((srapedDatas) => {
+  gdpr.structureScrapedDatas(srapedDatas).then((structuredDatas) => {
+    gdpr.createTOCS(structuredDatas).then((tocContents) => {
+      tocContents.forEach((tocContent) => {
+        let tocFile = path.join(__dirname, '..', 'web', 'dist', 'inc', 'parts', `toc_${tocContent.language.toLowerCase()}.html`);
+        fs.writeFile(tocFile, tocContent.toclist, (err) => {
+          if(err) throw err;
+          console.log(`TOC (${tocContent.language}) file has been written to "${tocFile}".`);
+        });
+      });
+    });
+  })
+}).catch((error) => {
+  console.log(error);
+});
--- toc-scraper/modules/gdpr.js
+++ toc-scraper/modules/gdpr.js
@@ -5,9 +5,10 @@ const scrapeIt = require("scrape-it");
 
 class GDPR {
 
-  constructor(language) {
-    this.languages = ['HU','EN','DE'];
-    this.language = (this.languages.indexOf(language) >= 0) ? language : 'HU';
+  constructor(url, languages) {
+    this.url = url;
+    this.languages = languages;
+    this.structuredEnData = {};
     this.names = {
       EN: {
         chapter:'chapter',
@@ -25,126 +26,208 @@ class GDPR {
         article:'artikel'
       }
     }
-    this.url = 'https://eur-lex.europa.eu/legal-content/' + this.language + '/TXT/?uri=celex:32016R0679';
   }
 
-  scrapeURL(url) {
+  scrapeURL(url, language) {
 
     return new Promise((resolve, reject) => {
       
-        // Scraping data
-        scrapeIt(url, {
-            sections: {
-              listItem: ".ti-section-1, .ti-art",
-              data: {
-                id: {
-                  attr: "id"
-                },
-                type: {
-                  how: "text"
-                }
-              }
+      // Scraping data
+      scrapeIt('http://' + url, {
+        sections: {
+          listItem: ".ti-section-1, .ti-art",
+          data: {
+            id: {
+              attr: "id"
             },
-            titles: {
-              listItem: ".ti-section-2, .sti-art",
-              data: {
-                title: {
-                  how: "text"
-                }
-              }
+            type: {
+              how: "text"
             }
-        }, (err, { data }) => {
-          return (err) ? reject(err) : resolve(data);
-        });
+          }
+        },
+        titles: {
+          listItem: ".ti-section-2, .sti-art",
+          data: {
+            title: {
+              how: "text"
+            }
+          }
+        }
+      }).then(({ data, response }) => {
+        return (response.statusCode != 200) ? reject(response.statusCode) : resolve({data:data, language:language});
+      })
     });
   }
 
-  structureContent(scrape) {
+  scrapeURLS() {
 
-    // Combining the two lists as [{id,type,title}]
-    let tocObject = [];
+    return new Promise((resolve, reject) => {
 
-    for (let i = 0; i < scrape['sections'].length; i++) {
-      let object = scrape['sections'][i];
-      object.title = scrape['titles'][i].title;
+      let scrapePromises = [];
 
-      tocObject.push(object);
-    }
+      this.languages.forEach((language) => {
+        let url = this.url.replace('{{lang}}', language);
+        scrapePromises.push(this.scrapeURL(url, language));
+      });
 
-    // Separating list based on sections
-    let chapterId = '';
-    let sectionId = null;
-    let structuredContent = {};
-    structuredContent.chapters = {};
-
-    for (let i = 0; i < tocObject.length; i++) {
-      let type = tocObject[i].type;
-      if(type.toLowerCase().indexOf(this.names[this.language].chapter) >= 0) {
-        structuredContent.chapters[i] = {
-          name : `${type} - ${tocObject[i].title}`,
-          id: tocObject[i].id
-        }
-        chapterId = i;
-        sectionId = null;
-      } else if(type.toLowerCase().indexOf(this.names[this.language].section) >= 0) {
-        sectionId = i;
-        structuredContent.chapters[chapterId]['sections'] = structuredContent.chapters[chapterId]['sections'] || {};
-        structuredContent.chapters[chapterId]['sections'][sectionId] = {
-          name : `${type} - ${tocObject[i].title}`,
-          id: tocObject[i].id
-        };
-      } else {
-        if(sectionId) {
-          structuredContent.chapters[chapterId].sections[sectionId]['articles'] = structuredContent.chapters[chapterId]['sections'][sectionId]['articles'] || {};
-          structuredContent.chapters[chapterId].sections[sectionId]['articles'][tocObject[i].id] = tocObject[i];
-        } else {
-          structuredContent.chapters[chapterId]['articles'] = structuredContent.chapters[chapterId]['articles'] || {};
-          structuredContent.chapters[chapterId]['articles'][tocObject[i].id] = tocObject[i];
-        }
-      }
-    }
-
-    return structuredContent;
+      Promise.all(scrapePromises).then((srapedDatas) => {
+        return resolve(srapedDatas);
+      }).catch((error) => {
+        return reject(error);
+      });
+    });
   }
 
-  formatContent(structuredContent) {
-    // Converting the list to HTML
-    let toclist = '<ul>\n';
+  structureScrapedData(scrapedData) {
 
-    for (let chapterId in structuredContent.chapters) {
-      let chapter = structuredContent.chapters[chapterId];
-      toclist += `<li><a href="${this.url}#${chapter.id}" target="_blank">${chapter.name}</a>\n`;
-        if(chapter.articles) {
-          toclist += '<ul>\n';
-          for (let articleId in chapter.articles) {
-            let article = chapter.articles[articleId];
-            toclist += `<li><a href="${this.url}#${article.id}" target="_blank">${article.title} - (${article.type})</a></li>\n`;
+    return new Promise((resolve, reject) => {
+      
+      // Combining the two lists as [{id,type,title}]
+      let tocObject = [];
+  
+      for (let i = 0; i < scrapedData.data['sections'].length; i++) {
+        let object = scrapedData.data['sections'][i];
+        object.title = scrapedData.data['titles'][i].title;
+  
+        tocObject.push(object);
+      }
+  
+      // Separating list based on sections (Chapter, Section, Article)
+      let chapterId = '';
+      let sectionId = null;
+      let structuredContent = {};
+      structuredContent.language = scrapedData.language;
+      structuredContent.chapters = {};
+  
+      for (let i = 0; i < tocObject.length; i++) {
+        let type = tocObject[i].type;
+        if(type.toLowerCase().indexOf(this.names[scrapedData.language].chapter) >= 0) {
+          structuredContent.chapters[i] = {
+            name : `${type} - ${tocObject[i].title}`,
+            id: tocObject[i].id
           }
-          toclist += '</ul>\n';
+          chapterId = i;
+          sectionId = null;
+        } else if(type.toLowerCase().indexOf(this.names[scrapedData.language].section) >= 0) {
+          sectionId = i;
+          structuredContent.chapters[chapterId]['sections'] = structuredContent.chapters[chapterId]['sections'] || {};
+          structuredContent.chapters[chapterId]['sections'][sectionId] = {
+            name : `${type} - ${tocObject[i].title}`,
+            id: tocObject[i].id
+          };
+        } else {
+          if(sectionId) {
+            structuredContent.chapters[chapterId].sections[sectionId]['articles'] = structuredContent.chapters[chapterId]['sections'][sectionId]['articles'] || {};
+            structuredContent.chapters[chapterId].sections[sectionId]['articles'][i] = tocObject[i];
+          } else {
+            structuredContent.chapters[chapterId]['articles'] = structuredContent.chapters[chapterId]['articles'] || {};
+            structuredContent.chapters[chapterId]['articles'][i] = tocObject[i];
+          }
         }
-        if(chapter.sections) {
-          toclist += '<ul>\n';
-          for (let sectionId in chapter.sections) {
-            let section = chapter.sections[sectionId];
-            toclist += `<li><a href="${this.url}#${section.id}" target="_blank">${section.name}</a>\n`;
-            if(section.articles) {
-              toclist += '<ul>\n';
-              for (let articleId in section.articles) {
-                let article = section.articles[articleId];
-                toclist += `<li><a href="${this.url}#${article.id}" target="_blank">${article.title} - (${article.type})</a></li>\n`;
-              }
-              toclist += '</ul>\n';
+      }
+
+      if(structuredContent.language === 'EN') {
+        this.structuredEnData.chapters = structuredContent.chapters;
+      }
+
+      resolve(structuredContent);
+    });
+  }
+
+  structureScrapedDatas(scrapedDatas) {
+
+    return new Promise((resolve, reject) => {
+
+      let structurePromises = [];
+
+      scrapedDatas.forEach((scrapedData) => {
+        structurePromises.push(this.structureScrapedData(scrapedData));
+      });
+
+      Promise.all(structurePromises).then((structuredDatas) => {
+        this.addEnglishTitles(structuredDatas);
+        return resolve(structuredDatas);
+      }).catch((error) => {
+        return reject(error);
+      });
+    });
+  }
+
+  addEnglishTitles(structuredDatas) {
+    let structuredDataEN = structuredDatas.filter(structuredData => structuredData.language === 'EN');
+
+    structuredDatas.forEach((structuredData) => {
+      
+    });
+  }
+
+  createTOC(structuredData) {
+
+    return new Promise((resolve, reject) => {
+
+      let href = `https://${this.url.replace('{{lang}}', structuredData.language).replace('TXT/HTML', 'TXT')}`;
+
+      // Converting the list to HTML
+      let toclist = '<ul>\n';
+  
+      for (let chapterId in structuredData.chapters) {
+        let chapter   = structuredData.chapters[chapterId];
+        let chapterEN = this.structuredEnData.chapters[chapterId];
+        toclist += `<li><a href="${href}#${chapter.id}" target="_blank" data-title-en="${chapterEN.name}">${chapter.name}</a>\n`;
+          if(chapter.articles) {
+            toclist += '<ul>\n';
+            for (let articleId in chapter.articles) {
+              let article   = chapter.articles[articleId];
+              let articleEN = chapterEN.articles[articleId];
+
+              toclist += `<li><a href="${href}#${article.id}" target="_blank" data-title-en="${articleEN.title} - (${articleEN.type})">${article.title} - (${article.type})</a></li>\n`;
             }
-            toclist += '</li>\n';
+            toclist += '</ul>\n';
           }
-          toclist += '</ul>\n';
-        }
-      toclist += '</li>\n';
-    }
+          if(chapter.sections) {
+            toclist += '<ul>\n';
+            for (let sectionId in chapter.sections) {
+              let section   = chapter.sections[sectionId];
+              let sectionEN = chapterEN.sections[sectionId];
+              toclist += `<li><a href="${href}#${section.id}" target="_blank" data-title-en="${sectionEN.name}">${section.name}</a>\n`;
+              if(section.articles) {
+                toclist += '<ul>\n';
+                for (let articleId in section.articles) {
+                  let article = section.articles[articleId];
+                  let articleEN = sectionEN.articles[articleId];
+                  toclist += `<li><a href="${href}#${article.id}" target="_blank" data-title-en="${articleEN.title} - (${articleEN.type})">${article.title} - (${article.type})</a></li>\n`;
+                }
+                toclist += '</ul>\n';
+              }
+              toclist += '</li>\n';
+            }
+            toclist += '</ul>\n';
+          }
+        toclist += '</li>\n';
+      }
+  
+      toclist += '</ul>\n';
+  
+      resolve({language:structuredData.language, toclist:toclist});
+    });
+  }
 
-    toclist += '</ul>\n';
+  createTOCS(structuredDatas) {
 
-    return toclist;
+    return new Promise((resolve, reject) => {
+
+      let tocPromises = [];
+
+      structuredDatas.forEach((structuredData) => {
+        tocPromises.push(this.createTOC(structuredData));
+      });
+
+      Promise.all(tocPromises).then((tocContents) => {
+        return resolve(tocContents);
+      }).catch((error) => {
+        return reject(error);
+      });
+    });
   }
   
 }
